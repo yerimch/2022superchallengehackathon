2f7320c9a3a690d95e1fbbb495e30ed3c7113cf8
yerimch <yerimchoi115@gmail.com>
2022-01-21 16:36:11 +0900

    real_last

--- app.py
+++ app.py
@@ -6,19 +6,15 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import gluonnlp as nlp
 import numpy as np
-from tqdm import tqdm, tqdm_notebook
 import random
-from flask import jsonify
-
+from flask import Flask, jsonify, request
 #kobert
 from kobert.utils import get_tokenizer
 from kobert.pytorch_kobert import get_pytorch_kobert_model
-
-#BERT 모델, Vocabulary 불러오기 필수
+app = Flask(__name__)
 bertmodel, vocab = get_pytorch_kobert_model()
 device="cpu"
 
-# KoBERT에 입력될 데이터셋 정리
 class BERTDataset(Dataset):
     def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
                  pad, pair):
@@ -32,24 +28,23 @@ class BERTDataset(Dataset):
         return (self.sentences[i] + (self.labels[i], ))
 
     def __len__(self):
-        return (len(self.labels))  
+        return (len(self.labels))
 
-# 모델 정의
-class BERTClassifier(nn.Module): ## 클래스를 상속
+class BERTClassifier(nn.Module):
     def __init__(self,
                  bert,
                  hidden_size = 768,
-                 num_classes=6,   ##클래스 수 조정##
+                 num_classes=5,
                  dr_rate=None,
                  params=None):
         super(BERTClassifier, self).__init__()
         self.bert = bert
         self.dr_rate = dr_rate
-                 
+
         self.classifier = nn.Linear(hidden_size , num_classes)
         if dr_rate:
             self.dropout = nn.Dropout(p=dr_rate)
-    
+
     def gen_attention_mask(self, token_ids, valid_length):
         attention_mask = torch.zeros_like(token_ids)
         for i, v in enumerate(valid_length):
@@ -58,14 +53,14 @@ class BERTClassifier(nn.Module): ## 클래스를 상속
 
     def forward(self, token_ids, valid_length, segment_ids):
         attention_mask = self.gen_attention_mask(token_ids, valid_length)
-        
+
         _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
         if self.dr_rate:
             out = self.dropout(pooler)
         return self.classifier(out)
 
 # Setting parameters
-max_len = 64
+max_len = 200
 batch_size = 32
 warmup_ratio = 0.1
 num_epochs = 20
@@ -73,16 +68,15 @@ max_grad_norm = 1
 log_interval = 100
 learning_rate =  5e-5
 
-## 학습 모델 로드
-#model = torch.load("/home/ubuntu/model/best_model_100epoch.pt",map_location=torch.device('cpu'))  # 전체 모델을 통째로 불러옴, 클래스 선언 필수
 
-#토큰화
+model = torch.load("/home/ubuntu/model/best_model_2epoch_maxlen_200.pt",map_location=torch.device('cpu'))
+
 tokenizer = get_tokenizer()
 tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
 
-def new_softmax(a) : 
-    c = np.max(a) # 최댓값
-    exp_a = np.exp(a-c) # 각각의 원소에 최댓값을 뺀 값에 exp를 취한다. (이를 통해 overflow 방지)
+def new_softmax(a) :
+    c = np.max(a)
+    exp_a = np.exp(a-c)
     sum_exp_a = np.sum(exp_a)
     y = (exp_a / sum_exp_a) * 100
     return np.round(y, 3)
@@ -90,11 +84,11 @@ def new_softmax(a) :
 def get_movie_list(emotion):
     if emotion==0:
         file="anger"
-    elif emotion==1 or emotion==3:
+    elif emotion==1:
         file="sadness"
     elif emotion==2:
         file="fear"
-    elif emotion==4:
+    elif emotion==3:
         file="surprise"
     else:
         file="joy"
@@ -104,51 +98,49 @@ def get_movie_list(emotion):
     with open(f"movie_recommendation/{file}") as f:
         for line in f.readlines():
             data.append(line.strip())
-    data=[x.split(",")[0:2] for x in data]
+    data=[x.split("\t")[0:2] for x in data]
     index=set()
     while len(index)!=3:
-        index.add(random.randint(0,len(data)))
+        index.add(random.randint(0,len(data)-1))
 
-    return [data[i] for i in index]
+    temp=[data[i] for i in index]
+    return temp
 
 def get_music_list(emotion):
-    if emotion==0:
+    if emotion==0 or emotion==3:
         file="angry"
-    elif emotion==1 or emotion==3:
+    elif emotion==1 or emotion==2:
         file="sad"
-    elif emotion==2:
-        file="nervous"
-    elif emotion==3:
-        file="hurt"
-    elif emotion==4:
-        file="panic"
     else:
         file="happy"
     file+=".txt"
     data=[]
 
-    with open(f"music_recommendation/{file}") as f:
+    with open(f"music_recommendation/{file}","r",encoding="utf-8") as f:
         for line in f.readlines():
             data.append(line.strip())
     data=[x.split(",")[0:2] for x in data]
+
     index=set()
+
     while len(index)!=3:
-        index.add(random.randint(0,len(data)))
+        index.add(random.randint(0,len(data)-1))
 
-    return [data[i] for i in index]
+    temp=[data[i] for i in index]
+    return temp
 
 
-# 예측 모델 설정
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    predict_sentence=request.files["contents"]
+    predict_sentence=request.get_json()["contents"]
 
     data = [predict_sentence, '0']
     dataset_another = [data]
 
     another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
     test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
-    
+
     model.eval()
     res=0
 
@@ -170,39 +162,38 @@ def predict():
             probability = []
             logits = np.round(new_softmax(logits), 3).tolist()
             for logit in logits:
-                print(logit)
                 probability.append(np.round(logit, 3))
             res=np.argmax(logits)
     movieList=get_movie_list(res)
     musicList=get_music_list(res)
 
     return jsonify({
-        "emotionType":res,
+        "emotionType":int(res),
         "movieList" : [
             {
-                "movieTitle":movieList[0][0],
-                "director":movieList[0][1],
+                "movieTitle":str(movieList[0][0]),
+                "director":str(movieList[0][1]),
                 },
             {
-                "movieTitle":movieList[1][0],
-                "director":movieList[1][1],
+                "movieTitle":str(movieList[1][0]),
+                "director":str(movieList[1][1]),
                 },
             {
-                "movieTitle":movieList[2][0],
-                "director":movieList[2][1],
+                "movieTitle":str(movieList[2][0]),
+                "director":str(movieList[2][1]),
                 }],
         "musicList":[
             {
-                "musicTitle":musicList[0][0],
-                "singer":musicList[0][1]
+                "musicTitle":str(musicList[0][0]),
+                "singer":str(musicList[0][1])
                 },
             {
-                "musicTitle":musicList[1][0],
-                "singer":musicList[1][1]
+                "musicTitle":str(musicList[1][0]),
+                "singer":str(musicList[1][1])
                 },
             {
-                "musicTitle":musicList[2][0],
-                "singer":musicList[2][1]
+                "musicTitle":str(musicList[2][0]),
+                "singer":str(musicList[2][1])
                 }]
         })
 
